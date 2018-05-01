e687f3788f451dcbba5b0a89ef1c2f82bbb13013
QianYongNian_Home <3321747055@qq.com>
2018-05-01 23:05:09 +0800

    启用非介入式验证需要JQuery

---
+++ WebApplication0419Home/WebApplication0501Home/Service1.svc.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Activation;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WebApplication0501Home
+{
+    [ServiceContract(Namespace = "")]
+    [AspNetCompatibilityRequirements(RequirementsMode = AspNetCompatibilityRequirementsMode.Allowed)]
+    public class Service1
+    {
+        // 要使用 HTTP GET，请添加 [WebGet] 特性。(默认 ResponseFormat 为 WebMessageFormat.Json)
+        // 要创建返回 XML 的操作，
+        //     请添加 [WebGet(ResponseFormat=WebMessageFormat.Xml)]，
+        //     并在操作正文中包括以下行:
+        //         WebOperationContext.Current.OutgoingResponse.ContentType = "text/xml";
+        [OperationContract]
+        public void DoWork()
+        {
+            // 在此处添加操作实现
+            return;
+        }
+
+        // 在此处添加更多操作并使用 [OperationContract] 标记它们
+        [OperationContract]
+        public string HelloWorld(string name)
+        {
+            return "Hi "+name+" Hello World From WCF HelloWorld";
+        }
+        public class Review
+        {
+            public int ID { get; set; }
+            public string Title { get; set; }
+
+        }
+        [OperationContract]
+        public List<Review> GetReviews()
+        {
+            List<Review> reviews = new List<Review>()
+            {
+                new Review{ID=1,Title="Nice Music" },
+                new Review{ID=2,Title="Holy Shit" }
+            };
+            return reviews;
+        }
+    }
+}
---
+++ WebApplication0419Home/WebApplication0501Home/WebForm1.aspx
@@ -0,0 +1,48 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="WebApplication0501Home.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title></title>
+    <%--<script src="Scripts/jquery-3.3.1.js"></script>--%>
+    <script type="text/javascript">
+        
+        function btn_click() {
+            Service1.GetReviews(callback);    
+        }
+        function callback(result) {
+            console.log(result);
+        }
+    </script>
+</head>
+<body>
+
+    <form id="form1" runat="server">
+        <div>
+        <asp:ScriptManager runat="server" ID="ScriptManager1">
+            <Services>
+                <asp:ServiceReference Path="~/Service1.svc" />
+            </Services>
+        </asp:ScriptManager>
+     
+            <asp:UpdatePanel ID="UpdatePanel1" runat="server">
+                <ContentTemplate>
+                    <asp:Label Text="Date" runat="server" ID="label1" /><br />
+                </ContentTemplate>
+                <Triggers>
+                    <asp:AsyncPostBackTrigger ControlID="Button1" />
+                </Triggers>
+            </asp:UpdatePanel>
+            <asp:Button Text="Click to Update" runat="server" OnClick="Button1_Click" ID="Button1" /><br />
+
+        </div>
+
+        <input type="button" id="button4" value="button4" onclick="btn_click();" />
+
+        
+
+    </form>
+</body>
+</html>
